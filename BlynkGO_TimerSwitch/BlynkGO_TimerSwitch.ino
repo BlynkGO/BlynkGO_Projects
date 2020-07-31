33724f74adfaecca8f6291d0945a132e22d5420d
TridentTD <prote2000@gmail.com>
2020-07-31 11:01:16 +0700

    update

--- BlynkGO_TimerSwitch/BlynkGO_TimerSwitch.ino
+++ BlynkGO_TimerSwitch/BlynkGO_TimerSwitch.ino
@@ -2,33 +2,34 @@
 
 #define BLYNKGO_KEY   "----------------"
 
-IMG_DECLARE(pic_blue_particle);
-FONT_DECLARE(BebasNeueBold_num_50);
-FONT_DECLARE(morkhor_40);
+IMG_DECLARE(pic_blue_particle);       // ประกาศรูปภาพ c file ก่อนใช้เสมอ
+FONT_DECLARE(BebasNeueBold_num_50);   // ประกาศเพิ่มฟอนต์ ก่อนใช้
+FONT_DECLARE(morkhor_40);             // ประกาศเพิ่มฟอนต์ ก่อนใช้
 
-GImage image(pic_blue_particle);
+GImage image(pic_blue_particle);  // วิตเจตรูปภาพ ทำเป็นพื้นหลัง โดยจากรูปจาก c file
+GKnob knob(image);                // วิตเจตปุ่มหมุน โดยมี layer แม่ คือ image 
+GLabel knob_lb;                   // วิตเจตข้อความ สำหรับบอกข้อมูล ว่า วิตเจตปุ่มหมุน ที่หมุนไป มีค่าเท่าไหร่
 
-GKnob knob(image);
-GLabel knob_lb;
-
-GRoller roller_h;
-GRoller roller_m;
-GRoller roller_s;
+GRoller roller_h;         // วิตเจตลูกกลิ้ง ทำเป็นตัวตั้งเวลา ของ ชั่วโมง
+GRoller roller_m;         // วิตเจตลูกกลิ้ง ทำเป็นตัวตั้งเวลา ของ นาที
+GRoller roller_s;         // วิตเจตลูกกลิ้ง ทำเป็นตัวตั้งเวลา ของ วินาที
 GLabel  lb_colon[2];
 
-GLed led(TFT_GREEN);
-GLabel label("เปิด",led);
+GLed led(TFT_GREEN);      // วิตเจ็ต led ทำเป็นปุ่มกด โดยมีสีเขียว
+GLabel label("เปิด",led);  // วิตเจ็ตข้อความ โดยมี layer แม่คือ led
 
-uint32_t bright = 255;
-uint32_t total_sec;
-GTimer timer;
+uint32_t total_sec;       // ค่าวินาทีทั้งหมด จาก ชม : นาที : วินาที  ที่เกิดจาก วิตเจ็ตลูกกลิ้ง GRoller
+GTimer timer;             // ตัวตั้งเวลาทำงาน
 
+// เมื่อ วิตเจ็ตลูกกลิ้ง GRoller มีการกลิ้งเปลี่ยนค่า ให้คำนวณ ค่าวินาทีรวมทั้งหมดออกมา
 void roller_onChanged(GWidget*w){
   total_sec = roller_h.selected().toInt()*3600 + roller_m.selected().toInt()*60 + roller_s.selected().toInt();
 }
 
+// สถานะของ วิตเจ็ต led
 enum {STATE_OFF, STATE_ON, STATE_START, STATE_PAUSE } state;
 
+// ฟังก์ชั่น สำหรับ reset ค่า
 void reset_timer(){
   label = "ปิด";
   total_sec = 0;
@@ -41,39 +42,44 @@ void setup() {
   Serial.begin(115200); Serial.println();
   BlynkGO.begin(BLYNKGO_KEY);
 
-  knob.color(TFT_RED);
-  knob.align(ALIGN_CENTER, -knob.width()/2-30, -10);
-  knob.range(0,255);
   
-  /* หากมีการเปลี่ยนแปลงค่าของ knob */
+  knob.color(TFT_RED);      // กำหนดสี knob
+  knob.align(ALIGN_CENTER, -knob.width()/2-30, -10);  // กำหนดตำแหน่งโดยใช้การจัดเรียง (align)
+  knob.range(0,255);        // กำหนดระยะของปุ่มหมุน knob มีค่าตั้งแต่ 0-255
+  
+  // หากมีการเปลี่ยนแปลงค่าของปุ่มหมุน  knob
   knob.onValueChanged([](GWidget* widget){
-    GKnob* pKnob = (GKnob*) widget;
-    bright = knob.value();
     knob_lb = "ความสว่าง : " + String(knob.value());
   });
 
+  // กำหนดข้อความ สำหรับบอกระดับของปุ่มหมุน knob
   knob_lb = "ความสว่าง : " + String(knob.value());
-  knob_lb.font(morkhor_40, TFT_WHITE);
-  knob_lb.align(knob, ALIGN_BOTTOM,0, 15);
+  knob_lb.font(morkhor_40, TFT_WHITE);      // กำหนด font และสีข้อความ
+  knob_lb.align(knob, ALIGN_BOTTOM,0, 15);  // จัดเรียงชิดล่าง ปุ่มหมุน ลงไปอีก 15 pixels
 
+  // กำหนดพื้นหลังของลูกลิ้งแบบโปร่งใส (opa=0), กำหนด font และ จำนวนแถวที่เห็นได้ คือ 1 บรรทัด
   roller_h.opa(0); roller_h.font(BebasNeueBold_num_50, TFT_WHITE);  roller_h.visible_rows(1);
   roller_m.opa(0); roller_m.font(BebasNeueBold_num_50, TFT_WHITE);  roller_m.visible_rows(1);
   roller_s.opa(0); roller_s.font(BebasNeueBold_num_50, TFT_WHITE);  roller_s.visible_rows(1);
 
+  // เพิ่ม 0-23 โดยเติม 0 นำหน้าด้วย ให้ ลูกกลิ้งชั่วโมง ...
+  // พร้อม กำหนด callback function เมื่อผู้ใช้หมุนเปลี่ยนค่ามีการเปลี่ยนแปลงเกิดขึ้น
   roller_h.options(0,23, true); roller_h.onValueChanged(roller_onChanged);
   roller_m.options(0,59, true); roller_m.onValueChanged(roller_onChanged);
   roller_s.options(0,59, true); roller_s.onValueChanged(roller_onChanged);
 
+  // ใส่เครื่องหมาย : ระหว่าง ชั่วโมง นาที และ วินาที
   lb_colon[0] = ":";  lb_colon[0].font(BebasNeueBold_num_50, TFT_WHITE);
   lb_colon[1] = ":";  lb_colon[1].font(BebasNeueBold_num_50, TFT_WHITE);
 
-  led.size(140,140);
-  led.align(knob, ALIGN_RIGHT, 60, -10);
-  led.onClicked([](GWidget* w){    
+  led.size(140,140);    // กำหนดขนาดวิตเจ็ต led
+  led.align(knob, ALIGN_RIGHT, 60, -10);  // จัดเรียงชิดขวาของ knob ถัดออกไป 60 pixel และขึ้นบน 10 pixel
+
+  // เมื่อมีการกด clicked เกิดขึ้น
+  led.onClicked([](GWidget* w){
     switch(state){
       case STATE_OFF:
         led.ON();
-        led.brightness(bright);
         state = (total_sec)? STATE_START : STATE_ON;
         break;
       case STATE_ON:
@@ -86,7 +92,6 @@ void setup() {
         break;
       case STATE_PAUSE:
         led.ON();
-        led.brightness(bright);
         state = (total_sec)? STATE_START : STATE_ON;
         break;
     }
@@ -94,6 +99,7 @@ void setup() {
 
     if( state == STATE_ON || state == STATE_START ) {
       if( total_sec && led.isON() ) {
+        // ตั้งเวลาทำงานทุกๆ 1 วินาที
         timer.setInterval(1000L,[](){
           static uint8_t h_prev, m_prev;
           total_sec--;
@@ -119,6 +125,7 @@ void setup() {
   });
 
   led.OFF();
+  // หากมีการกดค้างยาวเกิดขึ้นให้ทำงาน reset ค่า
   led.onLongPressed([](GWidget*w){
     reset_timer();
   });
