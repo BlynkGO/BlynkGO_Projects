2204013590d56576782720c0b973127ff3f3099d
TridentTD <prote2000@gmail.com>
2020-10-05 13:16:22 +0700

    Update BlynkGO_RiceChaoSticky.ino

--- BlynkGO_RiceChaoSticky/BlynkGO_RiceChaoSticky.ino
+++ BlynkGO_RiceChaoSticky/BlynkGO_RiceChaoSticky.ino
@@ -32,26 +32,25 @@ void setup() {
   Serial.begin(115200); Serial.println();
   BlynkGO.begin();
 
-  rice_chao.align(ALIGN_CENTER, - rice_chao.width()/2-10);
-  rice_sticky.align(ALIGN_CENTER,  rice_chao.width()/2 + 10);
+  rice_chao.align(ALIGN_CENTER, - rice_chao.width()/2-10);      // วางรูปข้าวเจ้าไว้ตรงกลาง แต่เลื่อนไป ทางซ้าย อีก...
+  rice_sticky.align(ALIGN_CENTER,  rice_chao.width()/2 + 10);   // วางรูปข้าวเหนียวไว้ตรงกลาง แต่เลื่อนไป ทางขวา อีก...
 
-//  rice_chao.filter(TFT_WHITE, 100);  
-  rice_chao.font(kanitlight_num_70, TFT_BLACK);
-  rice_sticky.linkStyle(rice_chao);
+  rice_chao.font(kanitlight_num_70, TFT_BLACK);                 // กำหนดฟอนต์ และ สีข้อความ (หากมีเลเยอร์ลูก เลเยอร์ลูกจะมาใช้ตัวนี้)
+  rice_sticky.linkStyle(rice_chao);                             // ให้ รูปข้าวเหนียว ก็ใช้ style จาก รูปของ ข้าวเจ้า (ประหยัดเม็ม)
   
-  lb_chao   = String(rice_chao_num);
-  lb_sticky = String(rice_sticky_num);
+  lb_chao   = String(rice_chao_num);                            // กำหนดข้อความ ด้วยจำนวน ของข้าวเจ้า
+  lb_sticky = String(rice_sticky_num);                          // กำหนดข้อความ ด้วยจำนวน ของข้าวเหนียว
 
-  lb_chao_info.align(rice_chao, ALIGN_TOP);
-  lb_sticky_info.align(rice_sticky, ALIGN_TOP);
+  lb_chao_info.align(rice_chao, ALIGN_TOP);                     // วางไว้ด้านบนของรูป ข้าวเจ้า
+  lb_sticky_info.align(rice_sticky, ALIGN_TOP);                 // วางไว้ด้านบนของรูป ข้าวเหนียว
 
-  rice_chao.clickable(true);
-  rice_chao.gesture(true);
-  rice_chao.onGesture([](GWidget*w){
-    gesture_dir_t gesture = w->touch_gesture();
+  rice_chao.clickable(true);                                    // ให้รับสัญญาณกดสัมผัสได้
+  rice_chao.gesture(true);                                      // ให้ปาดนิ้วได้
+  rice_chao.onGesture([](GWidget*w){                            // เมื่อมีสัมผัสแบบปาดนิ้วที่รูป ข้าวเจ้า
+    gesture_dir_t gesture = w->touch_gesture();                 // รูปแบบการปาดนิ้ว
     switch(gesture){
-      case GESTURE_DIR_TOP:     rice_chao_plus();    break;
-      case GESTURE_DIR_BOTTOM:  rice_chao_minus();   break;
+      case GESTURE_DIR_TOP:     rice_chao_plus();    break;     // ปาดขึ้นข้างบน
+      case GESTURE_DIR_BOTTOM:  rice_chao_minus();   break;     // ปาดลงข้างล่าง
     }
   });
   rice_sticky.clickable(true);
@@ -67,9 +66,9 @@ void setup() {
   btn_chao[0].size(60,60);
   btn_chao[0].color(TFT_RED);
   btn_chao[0].rounded(true);
-  btn_chao[0].onPressed(btn_pressed);
-  btn_chao[0].onPressing(btn_pressing);
-  btn_chao[0].onLongPressed(btn_long_pressed);
+  btn_chao[0].onPressed(btn_pressed);               // ปุ่มเมื่อมีการเริ่มกด
+  btn_chao[0].onPressing(btn_pressing);             // ปุ่มเมื่อมีการกดค้าง
+  btn_chao[0].onLongPressed(btn_long_pressed);      // ปุ่มเมื่อมีการกดค้างยาว
   
   btn_chao[1] = "+";
   btn_chao[1].size(60,60);
@@ -114,9 +113,9 @@ void btn_pressing(GWidget*w){
   timer = millis() + deboucing_time;
   
   if( btn_chao[1] == w)   { rice_chao_plus();   }else
-  if( btn_chao[0] == w)   { rice_chao_plus();   }else
+  if( btn_chao[0] == w)   { rice_chao_minus();  }else
   if( btn_sticky[1] == w) { rice_sticky_plus(); }else
-  if( btn_sticky[0] == w) { rice_sticky_minus(); }
+  if( btn_sticky[0] == w) { rice_sticky_minus();}
 }
 
 void btn_pressed(GWidget*w ){
@@ -124,9 +123,9 @@ void btn_pressed(GWidget*w ){
   if(timer >  millis()) return;
   timer = millis() + deboucing_time;
   if( btn_chao[1] == w)   { rice_chao_plus();   }else
-  if( btn_chao[0] == w)   { rice_chao_plus();   }else
+  if( btn_chao[0] == w)   { rice_chao_minus();  }else
   if( btn_sticky[1] == w) { rice_sticky_plus(); }else
-  if( btn_sticky[0] == w) { rice_sticky_minus(); }
+  if( btn_sticky[0] == w) { rice_sticky_minus();}
 }
 
 void btn_long_pressed(GWidget*w){
